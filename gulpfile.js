dbf761aed4a95b231fa5ac833811132b4d0f9f2e
Andrew Nichols <tandrewnichols@gmail.com>
2016-01-31 12:06:12 -0500

    What's in there?

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var sequence = require('gulp-sequence');
 require('file-manifest').generate('./gulp', { match: ['*.js', '!config.js'] });
-console.log(process.env.N_PREFIX);
 gulp.task('travis', sequence(['lint', 'cover', 'int'], 'codeclimate'));
 gulp.task('test', ['cover', 'int']);
 gulp.task('default', ['lint', 'test']);
