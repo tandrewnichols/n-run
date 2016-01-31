83bc9c0dd87a8fe7f5abec8f5e4508c8390f11b1
Andrew Nichols <tandrewnichols@gmail.com>
2016-01-31 16:51:49 -0500

    More logging

--- test/integration/test.js
+++ test/integration/test.js
@@ -5,10 +5,12 @@ var str = process.version;
 console.log(str);
 
 fs.stat(__dirname + '/output.txt', function(err, stat) {
+  console.log('FS.STAT', err);
   if (!err) {
     str = '\n' + str;
   }
   fs.appendFile(__dirname + '/output.txt', str, function(err) {
+    console.log('FS.APPENDFILE', err);
     if (err) {
       process.exit(1);
     } else {
